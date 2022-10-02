08024c9fbbbac42933df8a2c8e057dad0c3890ad
Elnar KIlmukhametov <soulmiker@gmail.com>
2022-10-02 13:50:06 +0300

    add test for TestNG

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -6,7 +6,7 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    public void testRemain() {
+    public void testRemain900() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
@@ -14,4 +14,21 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
 
     }
+    @org.testng.annotations.Test
+    public void testRemain1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testRemain1100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
 }
