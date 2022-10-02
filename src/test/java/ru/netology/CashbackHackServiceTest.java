5af523ef48cb33a763320664f0b439fe5328ae0e
Elnar KIlmukhametov <soulmiker@gmail.com>
2022-10-02 13:39:29 +0300

    add TestNG

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+
+    }
+}
